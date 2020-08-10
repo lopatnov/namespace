aaed5fcbf161cfe65af84632d97cd59ec6590bdb
Oleksandr Lopatnov <oleksandr@lopatnov.cv.ua>
2020-08-10 05:56:59 +0300

    Add test

--- tests/namespace.test.ts
+++ tests/namespace.test.ts
@@ -1,7 +1,14 @@
-import sampleLibrary from "../src/library";
+import { assert } from "console";
+import Namespace from "../src/namespace";
 
 describe("Base tests", () => {
   it("should create Library", () => {
-    expect(sampleLibrary()).toBe("Hello World!");
+    var x = new Namespace('Games.World');
+    var y: any = {};
+    var z = x.goto('Games.World');
+
+    x.applyTo(y, 'Hello');
+
+    expect(y.Hello.Games.World).toBe(z);
   });
 });
